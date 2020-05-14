c1666802716ff77973e5cc7f9bb0ad57bcf246de
Alex Alexandrovich <javafirst17@gmail.com>
2020-05-14 20:05:02 +0300

    Update cbrru.go

--- cbrru.go
+++ cbrru.go
@@ -24,3 +24,20 @@ type OneDay struct {
 		Value    string `xml:"Value"`
 	} `xml:"Valute"`
 } 
+
+type RangeOfDays struct {
+	XMLName    xml.Name `xml:"ValCurs"`
+	Text       string   `xml:",chardata"`
+	ID         string   `xml:"ID,attr"`
+	DateRange1 string   `xml:"DateRange1,attr"`
+	DateRange2 string   `xml:"DateRange2,attr"`
+	Name       string   `xml:"name,attr"`
+	Record     []struct {
+		Text    string `xml:",chardata"`
+		Date    string `xml:"Date,attr"`
+		ID      string `xml:"Id,attr"`
+		Nominal string `xml:"Nominal"`
+		Value   string `xml:"Value"`
+	} `xml:"Record"`
+} 
+
